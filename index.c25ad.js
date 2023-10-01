3e1ec5d9d0d167e31e3cd3faacef4ee8ca92a0b5
Nicholas <urkona@list.ru>
2023-10-01 18:30:21 +0300

    ss

---
+++ index.c25ad.js
@@ -0,0 +1,25 @@
+System.register(["./application.c0416.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationC0416Js) {
+      Application = _applicationC0416Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
